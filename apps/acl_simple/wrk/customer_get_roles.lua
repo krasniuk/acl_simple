b1a1554ff0abef5672b4f9212f4e1453fb526cab
Краснюк Михаил Андреевич <dn190901kma@dnh0-lvr806.ws.pbank.com.ua>
2023-06-01 04:53:39 -0500

    change run.sh

--- apps/acl_simple/wrk/customer_get_roles.lua
+++ apps/acl_simple/wrk/customer_get_roles.lua
@@ -1,5 +1,5 @@
 request = function()
-    path = "/admin"
+    path = "/customer"
     wrk.headers["Content-Type"] = "application/json; charset=utf-8"
     wrk.body = '{"auth":{"login":"lion_test","passhash":[113,16,237,164,208,158,6,42,165,228,163,144,176,165,114,172,13,44,2,32]},"parameters":{"method":"get_roles"}}'
     return wrk.format("POST", path)
--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
+appName=acl_simple
+
 pathFile=$(realpath $0)
 pathDir=$(dirname $pathFile)
 
 cd $pathDir
 make rel
-cd ./_build/prod/rel/acl_simple/bin/
-./acl_simple console
+cd ./_build/prod/rel/$appName/bin/
+./$appName console
